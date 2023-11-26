555409149b4752c188a33bea628de0cf12232f60
jzhupan <jzhupan@gmail.com>
2023-11-25 22:12:21 -0500

    Adding placeholder footer

---
+++ react-project/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/react";
+
+const Footer = () => {
+  return (
+    <div>
+      <Box bg="tomato" w="100%" p={4} color="white">
+        This is the Footer
+      </Box>
+    </div>
+  );
+};
+
+export default Footer;
