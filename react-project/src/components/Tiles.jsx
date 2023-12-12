bf0a027161a75444022adcd43f78d7b17cbcf8b7
jzhupan <jzhupan@gmail.com>
2023-12-12 15:11:23 -0500

    Brought the fun animations for all the modals excluding the ones with the custom GIF animations.

--- react-project/src/components/Tiles.jsx
+++ react-project/src/components/Tiles.jsx
@@ -113,7 +113,7 @@ function Tiles() {
     setIsModalOpen(false);
   };
 
-  const animatedSquares = [5, 6];
+  const animatedSquares = [0, 1, 3, 5, 6, 7];
   const colorChangeSquares = [0, 1, 2, 3, 4, 5, 6, 7];
   const shuffledAnimations = shuffleArray([...animations]);
   const shuffledColors = shuffleArray([...colors]);
