bc43759b1607b075aed26e5a452d6406d954aa1b
jzhupan <jzhupan@gmail.com>
2023-12-21 18:56:16 -0500

    Added Accessibility, moved robots.txt file to correct directory, testing SEO score

--- react-project/src/components/Footer.jsx
+++ react-project/src/components/Footer.jsx
@@ -21,13 +21,25 @@ const Footer = () => {
         align={"center"}
       >
         <HStack spacing={4} justifyContent="center" w={"100%"}>
-          <Link href="https://www.linkedin.com/in/jenniezp/" isExternal>
+          <Link
+            href="https://www.linkedin.com/in/jenniezp/"
+            isExternal
+            alt="linkedin profile url"
+          >
             <Icon as={FaLinkedin} w={6} h={6} className="icons" />
           </Link>
-          <Link href="https://github.com/jzhupan" isExternal>
+          <Link
+            href="https://github.com/jzhupan"
+            isExternal
+            alt="github profile url"
+          >
             <Icon as={FaGithub} w={6} h={6} className="icons" />
           </Link>
-          <Link href="https://www.jenniezp.art/" isExternal>
+          <Link
+            href="https://www.jenniezp.art/"
+            isExternal
+            alt="art portfolio url"
+          >
             <Icon as={FaPaintBrush} w={6} h={6} className="icons" />
           </Link>
         </HStack>
