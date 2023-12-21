f1b67631853e07471f56552cf18b2ddd318c0718
jzhupan <jzhupan@gmail.com>
2023-12-21 18:53:56 -0500

    Testing SEO score

--- react-project/src/components/Tiles.jsx
+++ react-project/src/components/Tiles.jsx
@@ -191,7 +191,6 @@ function Tiles() {
         isOpen={isModalOpen}
         onClose={handleCloseModal}
         modalContent={modalContents[selectedSquare]}
-        modalHeader={modalHeaders[selectedSquare]}
       />
     </ChakraProvider>
   );
